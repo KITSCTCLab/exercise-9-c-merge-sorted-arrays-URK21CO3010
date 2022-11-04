1c303054a2386560e90134f21b9570bd864b2ace
URK21CO3010 <110015139+URK21CO3010@users.noreply.github.com>
2022-11-04 21:37:35 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,30 +2,32 @@ from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   # Write code here
-    array = [0 for i in range(m + n)]
-    i = 0
-    j = 0
-    k = 0
-    while i < m and j < n:
-        if nums1[i] <= nums2[j]:
-            array[k] = nums1[i]
-            i += 1
-        else:
-            array[k] = nums2[j]
-            j += 1
-        k += 1
-    if i >= m:
-        while j < n:
-            array[k] = nums2[j]
-            j += 1
-            k += 1
+  array = [0 for i in range(m + n)]
+  
+  i = 0
+  j = 0
+  k = 0
+  
+  while i <= m and j <= n:
+    if nums1[i] < nums2[j]:
+      array[k] = nums1[i]
+      i += 1
     else:
-        while i < m:
-            array[k] = nums1[i]
-            i += 1
-            k += 1
-    for i in range(len(array)):
-      nums1[i] = array[i]
+      array[k] = nums2[j]
+      j += 1
+    k += 1
+  
+  while i <= m:
+    array[k] = nums1[i]
+    i += 1
+    k += 1
+  
+  while j <= n:
+    array[k] = nums2[j]
+    2 += 1
+    k += 1
+  
+  nums1 = array
 
 
 # Do not change the following code
